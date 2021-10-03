f87d2f4c59949318cf6ddfdd7b3c068502dd9695
YY <yy.ilvry@gmail.com>
2021-10-03 16:39:59 -0400

    1.0 local draft functional

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, abort
-app = Flask(__name__)
+from flask import Flask, render_template, abort, request, redirect, url_for, Response
+from flask_caching import Cache
+import cv2
+from blink_detection import face_detection
+
 
 @app.route('/')
-def open_mainpage():
+def index():
     return render_template('baseExt.html')
+
+@app.errorhandler(404)
+def open_dreampage(error):
+    return redirect(url_for('index')), 404
   
 @app.route('/<path:path>')
 def open_paths(path):
@@ -12,9 +19,50 @@ def open_paths(path):
   else:
     abort(404)
 
-@app.errorhandler(404)
-def open_dreampage(error):
-    return render_template('baseExt.html'), 404
+# generate frame by frame from camera
+def gen_frames():
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            eye_open, frame = face_detection(frame, face_cascade, eye_cascade)
+            cache.set("eye_open", eye_open)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# generate frame by frame from two images
+def get_image():
+    while True:
+      if cache.get("eye_open"):
+        img = eye_open_img
+      else:
+        img = eye_close_img
+      yield(b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n'+ img + b'\r\n')
+
+@app.route("/img_feed")
+def img_feed():
+    return Response(get_image(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+  
 if __name__ == '__main__':
+    cache = Cache(app)
+    cache.set("eye_open", True)
+    camera = cv2.VideoCapture(0)  # web camera
+
+    # init the face and eye cascade classifiers from xml files
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+    eye_open_img = open('static/img/open.png', 'rb').read()
+    eye_close_img = open('static/img/close.png', 'rb').read()
+    
     app.run(debug=True)
---
+++ blink_detection.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+
+def face_detection(img, face_cascade, eye_cascade):
+  eye_open = True
+
+  #Coverting the recorded image to grayscale
+  gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+  #Applying filter to remove impurities
+  gray = cv2.bilateralFilter(gray,5,1,1)
+  #Detecting the face for region of image to be fed to eye classifier
+  faces = face_cascade.detectMultiScale(gray, 1.3, 5,minSize=(200,200))
+
+  if(len(faces)>0):
+    for (x,y,w,h) in faces:
+      img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),4)
+      
+      #roi_face is face which is input to eye classifier
+      roi_face = gray[y:y+h,x:x+w]
+      roi_face_clr = img[y:y+h,x:x+w]
+      eyes = eye_cascade.detectMultiScale(roi_face,1.3,5,minSize=(50,50))
+      
+      #Examining the length of eyes object for eyes
+      if(len(eyes)>=2):
+          cv2.putText(img, "eyes open", (70,70), cv2.FONT_HERSHEY_PLAIN, 1.5, (159,50,0),2)
+      else: 
+        cv2.putText(img, "eyes closed", (70,70), cv2.FONT_HERSHEY_PLAIN, 1.5, (50,159,255),2)
+        eye_open = False
+  else:
+    cv2.putText(img,"no face detected",(100,100),cv2.FONT_HERSHEY_PLAIN, 1.5, (255,255,255),2)
+
+  # add filter to image
+  img = inc_brightness(img)
+  return eye_open, img
+
+def inc_brightness(img):
+  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  h, s, v = cv2.split(hsv)
+
+  added_brightness = 100
+  lim = 255 - added_brightness
+  v[v > lim] = 255
+  v[v <= lim] += added_brightness
+  
+  lowered_saturation = 100
+  s[s > lowered_saturation] -= lowered_saturation
+  s[s <= lowered_saturation] = 0
+
+  final_hsv = cv2.merge((h, s, v))
+  img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+  return img
